9961f7f77a5d7d9f038c9882f4a0ad2d496b01ec
hacklover <info@hacklover.net>
2021-07-05 06:10:35 +0200

    Initial commit

---
+++ modules/StatusMenu/components/battery/SystemBarStatus.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <slot name="prepend" />
+
+    <SystemBarStatusBattery />
+
+    <slot name="append" />
+  </div>
+</template>
+
+<script setup>
+import SystemBarStatusBattery from "./SystemBarStatusBattery";
+</script>

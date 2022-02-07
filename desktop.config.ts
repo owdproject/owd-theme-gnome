04d53f549329c89ba7a0a747fe3c910a89e800b9
hacklover <info@hacklover.net>
2022-02-07 10:01:26 +0100

    Update theme config to owd 2.0.0-beta.4

--- desktop.config.ts
+++ desktop.config.ts
@@ -9,73 +9,83 @@ import DesktopStatus from "./modules/DesktopStatus";
 // plugins
 import Vuetify from "owd-vuetify-plugin/src"
 
-// i18n
-import locales from './locales'
+// locales
+import locales from "./locales"
 
 export default {
-  name: 'gnome',
+    name: 'gnome',
+    compatibility: '2.0.0-beta.4',
 
-  component: Desktop,
+    variants: {
+        'adwaita-dark': 'assets/styles/variants/adwaita-dark/variables.scss',
+        'adwaita-light': 'assets/styles/variants/adwaita-light/variables.scss'
+    },
 
-  modules: [
-    DesktopLauncher,
-    DesktopNotification,
-    DesktopStatus
-  ],
+    options: {
+        Logo: {
+            enabled: true
+        },
+        Dock: {
+            enabled: true
+        },
+        SystemBar: {
+            enabled: true,
+            position: 'top',
+            icons: {
+                'battery': 'mdi mdi-battery',
+                'battery-0': 'mdi mdi-battery-alert-variant-outline',
+                'battery-20': 'mdi mdi-battery-20',
+                'battery-40': 'mdi mdi-battery-40',
+                'battery-60': 'mdi mdi-battery-60',
+                'battery-80': 'mdi mdi-battery-80',
+                'battery-100': 'mdi mdi-battery'
+            }
+        },
+        ApplicationMenu: {
+            categoryAppsTriggerType: 'mouseover'
+        },
+        NotificationMenu: {
+            menu: {
+                dateFormat: 'MMM D',
+                timeFormat: 'HH:mm'
+            },
+            calendar: {
+                header: {
+                    dayOfWeekFormat: 'dddd',
+                    dateFormat: 'MMMM D YYYY'
+                }
+            },
+            floatingNotification: {
+                max: 2,
+                duration: 8000
+            }
+        },
 
-  plugins: [
-    Vuetify
-  ],
 
-  options: {
-    Logo: {
-      enabled: true
+        Window: {
+            icons: {
+                minimize: 'mdi mdi-window-minimize',
+                maximize: 'mdi mdi-window-maximize',
+                fullscreen: 'mdi mdi-fullscreen',
+                close: 'mdi mdi-window-close',
+                external: 'mdi mdi-open-in-new'
+            },
+        },
     },
-    Dock: {
-      enabled: true
-    },
-    Window: {
-      icons: {
-        minimize: 'mdi mdi-window-minimize',
-        maximize: 'mdi mdi-window-maximize',
-        fullscreen: 'mdi mdi-fullscreen',
-        close: 'mdi mdi-window-close',
-        external: 'mdi mdi-open-in-new'
-      }
-    },
-    SystemBar: {
-      enabled: true,
-      position: 'top',
-      icons: {
-        'battery': 'mdi mdi-battery',
-        'battery-0': 'mdi mdi-battery-alert-variant-outline',
-        'battery-20': 'mdi mdi-battery-20',
-        'battery-40': 'mdi mdi-battery-40',
-        'battery-60': 'mdi mdi-battery-60',
-        'battery-80': 'mdi mdi-battery-80',
-        'battery-100': 'mdi mdi-battery'
-      }
-    },
-    ApplicationMenu: {
-      categoryAppsTriggerType: 'mouseover'
-    },
-    NotificationMenu: {
-      menu: {
-        dateFormat: 'MMM D',
-        timeFormat: 'HH:mm'
-      },
-      calendar: {
-        header: {
-          dayOfWeekFormat: 'dddd',
-          dateFormat: 'MMMM D YYYY'
-        }
-      },
-      floatingNotification: {
-        max: 2,
-        duration: 8000
-      }
-    }
-  },
 
-  locales
+    locales,
+
+    components: {
+        'Desktop': Desktop
+    },
+
+    modules: [
+        DesktopLauncher,
+        DesktopNotification,
+        DesktopStatus
+    ],
+
+    plugins: [
+        Vuetify
+    ],
 }
